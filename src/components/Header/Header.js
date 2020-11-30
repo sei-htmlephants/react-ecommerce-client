7c96058dc8e5f352394ae8a1e96aeec4521f0c8a
mcidoni <matthewcidoni@gmail.com>
2020-11-30 15:53:08 -0500

    minor cosmetic text changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,8 @@ const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#change-password">change password</Nav.Link>
     <Nav.Link href="#sign-out">sign out</Nav.Link>
-    <Nav.Link href="#create-purchase">create purchase</Nav.Link>
-    <Nav.Link href="#index-purchases">index purchase</Nav.Link>
+    <Nav.Link href="#create-purchase">create purchase test</Nav.Link>
+    <Nav.Link href="#index-purchases">purchase history</Nav.Link>
   </Fragment>
 )
 
