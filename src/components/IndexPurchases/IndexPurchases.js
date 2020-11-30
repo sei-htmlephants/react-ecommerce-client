3bcdd1aaf1d311a1bcbc7b5e2a71ee02a92a7676
sidhantmathur <sidhant185@gmail.com>
2020-11-30 09:18:11 -0600

    add componentDidMount
    Index function now working.
    
    Co-authored-by: codessi <johansuh@gmail.com>
    Co-authored-by: mcidoni <matthewcidoni@gmail.com>

--- src/components/IndexPurchases/IndexPurchases.js
+++ src/components/IndexPurchases/IndexPurchases.js
@@ -12,22 +12,22 @@ class IndexPurchases extends Component {
     }
   }
 
-  onIndexPurchases = event => {
-    event.preventDefault()
-
-    const { msgAlert, history, user } = this.props
+  componentDidMount () {
+    const { msgAlert, user } = this.props
 
     indexPurchases(user)
-      // .then(console.log('asdfasd'))
+
+      .then(res => {
+        console.log(res)
+        this.setState({ purchases: res.data.purchases })
+      })
+
       .then(() => msgAlert({
         heading: 'Index Purchases Success',
         message: messages.indexPurchasesSuccess,
         variant: 'success'
       }))
-      .then(() => history.push('/'))
-      // .then(res => { console.log(res) })
-      // .then(res => setPurchases(res.data.purchases))
-      // .then(() => this.setState({ purchases: this.state.purchases }))
+
       .catch(error => {
         msgAlert({
           heading: 'Index Purchases Failed with error: ' + error.message,
@@ -39,14 +39,11 @@ class IndexPurchases extends Component {
 
   render () {
     let purchaseJsx
-    // if our data has not come in (loading)
     if (!this.state.purchases) {
       purchaseJsx = 'Loading...'
     } else if (this.state.purchases.length === 0) {
-      // if we have data but it's empty - no purchases
       purchaseJsx = 'No purchases to display :('
     } else {
-      // if we have data to show! purchases
       purchaseJsx = this.state.purchases.map(purchase => (
         <div key={purchase._id}>
           {/* <Link to={'/purchases/' + purchase._id}> */}
