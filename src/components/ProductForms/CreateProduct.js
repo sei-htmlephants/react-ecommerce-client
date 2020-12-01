9349ad3f247cfd5fdb9457298e9ca4039e2a0c15
mcidoni <matthewcidoni@gmail.com>
2020-12-01 11:03:31 -0500

    further changes to createpurchase.js

--- src/components/ProductForms/CreateProduct.js
+++ src/components/ProductForms/CreateProduct.js
@@ -1,44 +1,89 @@
-render () {
-  const { purchaseProduct, productPrice } = this.state
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
-  return (
-    <div className="row">
-      <div className="col-sm-10 col-md-8 mx-auto mt-5">
-        <h3>Create Purchase</h3>
-        <Form onSubmit={this.onCreatePurchase}>
-          <Form.Group controlId="purchaseProduct">
-            <Form.Label>Product Name</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              name="purchaseProduct"
-              value={purchaseProduct}
-              placeholder="Enter product's name"
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group controlId="password">
-            <Form.Label>Product Price</Form.Label>
-            <Form.Control
-              required
-              name="productPrice"
-              value={productPrice}
-              type="number"
-              placeholder="Enter product's price"
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Button
-            variant="primary"
-            type="submit"
-          >
-            Submit
-          </Button>
-        </Form>
+import { createPurchase } from '../../api/purchases'
+import messages from '../AutoDismissAlert/messages'
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+class CreatePurchase extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      purchaseProduct: '',
+      productPrice: ''
+    }
+  }
+
+  handleChange = event => this.setState({
+    [event.target.name]: event.target.value
+  })
+
+  onCreatePurchase = event => {
+    event.preventDefault()
+
+    const { msgAlert, history, user } = this.props
+
+    createPurchase(this.state, user)
+      .then(() => msgAlert({
+        heading: 'Create Purchase Success',
+        message: messages.createPurchaseSuccess,
+        variant: 'success'
+      }))
+      .then(() => history.push('/'))
+      .catch(error => {
+        this.setState({ purchaseProduct: '', productPrice: '' })
+        msgAlert({
+          heading: 'Purchase Creation Failed with error: ' + error.message,
+          message: messages.signUpFailure,
+          variant: 'danger'
+        })
+      })
+  }
+
+  render () {
+    const { purchaseProduct, productPrice } = this.state
+
+    return (
+      <div className="row">
+        <div className="col-sm-10 col-md-8 mx-auto mt-5">
+          <h3>Create Purchase</h3>
+          <Form onSubmit={this.onCreatePurchase}>
+            <Form.Group controlId="purchaseProduct">
+              <Form.Label>Product Name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="purchaseProduct"
+                value={purchaseProduct}
+                placeholder="Enter product's name"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Product Price</Form.Label>
+              <Form.Control
+                required
+                name="productPrice"
+                value={productPrice}
+                type="number"
+                placeholder="Enter product's price"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Button
+              variant="primary"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default withRouter(CreatePurchase)
