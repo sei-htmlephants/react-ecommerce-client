e7d5c886425084ff8ee3767c389ce5748f0a1c32
codessi <johansuh@gmail.com>
2020-12-01 12:29:41 -0800

    add buybutton

--- src/components/ProductForms/IndexProducts.js
+++ src/components/ProductForms/IndexProducts.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
+import { createPurchase } from '../../api/purchases'
 import { indexProducts } from '../../api/products'
 import messages from '../AutoDismissAlert/messages'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 class IndexProducts extends Component {
   constructor () {
@@ -39,6 +41,28 @@ class IndexProducts extends Component {
       })
   }
 
+  onCreatePurchase = event => {
+    event.preventDefault()
+
+    const { msgAlert, history, user } = this.props
+
+    createPurchase(this.state, user)
+      .then(() => msgAlert({
+        heading: 'Create Purchase Success',
+        message: messages.createPurchaseSuccess,
+        variant: 'success'
+      }))
+      .then(() => history.push('/'))
+      .catch(error => {
+        this.setState({ purchaseProduct: '', productPrice: '' })
+        msgAlert({
+          heading: 'Purchase Creation Failed with error: ' + error.message,
+          message: messages.signUpFailure,
+          variant: 'danger'
+        })
+      })
+  }
+
   render () {
     let productJsx
     if (!this.state.products) {
@@ -60,15 +84,40 @@ class IndexProducts extends Component {
                 You last updated this product on: {product.createdAt.slice(0, -14)}
             </footer> */}
             <Button variant="outline-primary" href={'/#/products/' + product._id}>See More</Button>
+            <Form onSubmit={this.onCreatePurchase}>
+              <Form.Group controlId="purchaseProduct">
+                {/* <Form.Label>Product Name</Form.Label> */}
+                <Form.Control
+                  required
+                  type="hidden"
+                  name="purchaseProduct"
+                  // value={product.productName}
+                  value="hello"
+                  placeholder="Enter product's name"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="productPrice">
+                {/* <Form.Label>Product Price</Form.Label> */}
+                <Form.Control
+                  required
+                  name="productPrice"
+                  // value={product.productPrice}
+                  value="1234"
+                  type="hidden"
+                  placeholder="Enter product's price"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="submit"
+              >Buy</Button>
+            </Form>
+            {/* <Button variant="outline-primary" onClick={this.buyProduct(product._id)}>Buy</Button> */}
           </Card.Body>
         </Card>
 
-        // <div key={product._id}>
-        //   <Link to={'/products/' + product._id}>
-        //     <h3>{product.productProduct}</h3>
-        //   </Link>
-        //   <p>${product.productPrice}</p>
-        // </div>
       ))
     }
 
