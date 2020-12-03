2f80fd579bef15394be9392a9d312efd17442492
sidhantmathur <sidhant185@gmail.com>
2020-12-02 19:39:44 -0600

    fix url in dev branch

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://thawing-beach-03969.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
