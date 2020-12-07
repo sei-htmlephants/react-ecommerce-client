6ddbc0d3054e1e0d041d790309b7a52b37527255
mcidoni <matthewcidoni@gmail.com>
2020-12-07 16:29:01 -0500

    remove console.logs

--- src/components/ProductForms/ShowProduct.js
+++ src/components/ProductForms/ShowProduct.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 import { showProduct, deleteProduct } from '../../api/products'
 
@@ -61,36 +61,35 @@ const ShowProduct = (props) => {
       {/* {user.email === 'admin@admin' ? (<Fragment>youre an admin</Fragment>) : (<Fragment>youre a user</Fragment>)} */}
 
       {product ? (
-        <Fragment>
-          <Container>
-            <Card key={product._id} className="Card">
-              <Row>
-                <Col sm={6}>
-                  <Card.Img variant="top" src={product.productImages} />
-                </Col>
-                <Col sm={6}>
-                  <Card.Body>
-                    <Card.Title>{product.productName}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">${product.productPrice}</Card.Subtitle>
-                    <Card.Text>
-                      {product.productDescription}
-                    </Card.Text>
-                    <HiddenCreate
-                      user={user}
-                      msgAlert={msgAlert}
-                      history={history}
-                      productPrice={product.productPrice}
-                      purchaseProduct={product.productName}
-                    />
-                    <p></p>
-                    <Button variant="danger" onClick={handleDelete}>Delete</Button>{' '}
-                    <Button href={'#product-update/' + product._id}>Update Product</Button>{' '}
-                  </Card.Body>
-                </Col>
-              </Row>
-            </Card>
-          </Container>
-        </Fragment>
+        <Container>
+          <Card key={product._id} className="Card">
+            <Row>
+              <Col sm={6}>
+                <Card.Img variant="top" src={product.productImages} />
+              </Col>
+              <Col sm={6}>
+                <Card.Body>
+                  <Card.Title>{product.productName}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">${product.productPrice}</Card.Subtitle>
+                  <Card.Text>
+                    {product.productDescription}
+                  </Card.Text>
+                  <HiddenCreate
+                    user={user}
+                    msgAlert={msgAlert}
+                    history={history}
+                    productPrice={product.productPrice}
+                    purchaseProduct={product.productName}
+                  />
+                  <p></p>
+                  <Button variant="danger" onClick={handleDelete}>Delete</Button>{' '}
+                  <Button href={'#product-update/' + product._id}>Update Product</Button>{' '}
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        </Container>
+
       ) : 'Loading...'}
     </div>
   )
